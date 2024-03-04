531ba37f08aed3006492bd6e3df4fa17501dd6d3
akshaykid <162188219+akshaykid@users.noreply.github.com>
2024-03-04 15:03:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+# Imports go at the top
+from cyberbot import *
+from microbit import *
+import radio
+radio.on()
 
+while True:
+    
+    if button_a.is_pressed():
+        audio.play(audio.SoundEffect())
+        radio.send('A')
+        
+    elif button_b.is_pressed():   
+        audio.play(audio.SoundEffect())
+        radio.send('B')
+
+    if radio.receive() == "A":
+        display.show("A")
+
+    if radio.receive() == "B":
+       display.show("B")
+
+print("x")
