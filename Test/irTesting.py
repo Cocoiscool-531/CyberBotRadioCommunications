303b2e8bcaeda7c714db9603b5d9a51525bcf744
Cocoiscool-531 <119710842+Cocoiscool-531@users.noreply.github.com>
2024-03-21 14:18:04 -0400

    Update irTesting.py

--- Test/irTesting.py
+++ Test/irTesting.py
@@ -4,7 +4,7 @@ from time import *
 
 freqA = 15_000
 freqB = 20_000
-freq = 0
+freq = 1
 sleep = 0
 us_s = 1000000
 
@@ -12,20 +12,26 @@ def transmit():
     while True:
         if button_a.is_pressed():
             freq = freqA
+            display.show("A")
         elif button_b.is_pressed():
             freq = freqB
+            display.show("B")
         else:
-            freq = 0
+            freq = 1
+            display.clear()
 
         sleep = us_s / freq
     
-        if freq != 0:
+        if freq != 1:
             sleep_us(int(sleep))
+            bot(5).write_digital(1)
             bot(0).write_digital(1)
             sleep_us(int(sleep))
+            bot(5).write_digital(0)
             bot(0).write_digital(0)
         else:
             bot(0).write_digital(0)
+            bot(5).write_digital(0)
 
 def recieve():
     while True:
@@ -38,3 +44,9 @@ def recieve():
             display.show("B")
         else:
             display.clear()
+
+def run(type):
+    if type == "T":
+        transmit()
+    if type == "R":
+        transmit()
